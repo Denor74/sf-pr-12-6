3c11d5af93d8a47847b4b5501b204545761744ae
Denor74 <den74@inbox.ru>
2023-03-28 21:53:46 +0500

    array fullName

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 // data-arr.php хранит массив $example_persons_array
 require 'data-arr.php';
-print_r($example_persons_array);
+//print_r($example_persons_array);
 
-// создаем массив из трех элементов "Фамилия" "Имя" "Отчество" из значения 'fullname' массива $example_persons_array
-function getPartsFromFullname(arrFullname) {
+// создаем массив только со начением  fullname из массива $example_persons_array
 
+$fullName = [];
+foreach($example_persons_array as $fullname) {
+    $fullName[] = $fullname['fullname'];
 }
+//print_r($fullName);
+print_r($fullName[0]);
+
+// принимает строку ФИО и возвращает массив из трех элементов Фамилия, Ися, Отчество
+function getPartsFromFullname() {}
 
 // принимает три строки "Фамилия" "Имя" "Отчество" и склеивает в одну
 function getFullnameFromParts() {
