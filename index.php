fadaef74baf98a8f71acb91a3061b6b07648e9dd
Denor74 <den74@inbox.ru>
2023-03-28 16:02:24 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// data-arr.php хранит массив $example_persons_array
+require 'data-arr.php';
+print_r($example_persons_array);
+
+// создаем массив из трех элементов "Фамилия" "Имя" "Отчество" из значения 'fullname' массива $example_persons_array
+function getPartsFromFullname(arrFullname) {
+
+}
+
+// принимает три строки "Фамилия" "Имя" "Отчество" и склеивает в одну
+function getFullnameFromParts() {
+
+}
+
+?>
